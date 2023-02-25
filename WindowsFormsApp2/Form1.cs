be2b35e4bc47fd935ced54c0ab1a2ff12c1fac8f
ana26V <anamariavoichescu26@gmail.com>
2023-02-25 23:44:08 +0200

    ok

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -17,6 +17,47 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            
+
+            string words = "";
+
+            string[] units = { "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua", "zece", "unsprezece", "doisprezece", "treisprezece", "paisprezece", "cincisprezece", "saisprezece", "saptesprezece", "optsprezece", "nouasprezece" };
+            string[] tens = { "douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci" };
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " mii ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " sute ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "si ";
+
+                if (number < 20)
+                    words += units[number - 1];
+                else
+                {
+                    words += tens[number / 10 - 2];
+                    if ((number % 10) > 0)
+                        words += "-" + units[number % 10 - 1];
+                }
+            }
+
+            return words;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -24,8 +65,12 @@ namespace WindowsFormsApp2
             double sbrut = double.Parse(textBox1.Text);
             double CFS_angajat = (sbrut / 1000) * 5;
             label1.Text= CFS_angajat.ToString();
+           
+           
+            string words = NumberToWords((int)sbrut);
             string aux = textBox1.Text;
-            MessageBox.Show("Ati introdus" + aux);
+            MessageBox.Show("Ati introdus " + words);
+           
         }
     }
 }
