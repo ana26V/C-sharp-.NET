354798e9ac49b20fab0af9fab8e72f2c87b152e3
ana26V <anamariavoichescu26@gmail.com>
2023-03-05 20:47:54 +0200

    Alta app

---
+++ X si 0/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace X_si_Zero_eu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.76335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.23665F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 298F));
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(267, 207);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 223F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 636);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Lavender;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Indigo;
+            this.button9.Location = new System.Drawing.Point(588, 415);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(287, 200);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Lavender;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Indigo;
+            this.button8.Location = new System.Drawing.Point(282, 415);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(295, 200);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Lavender;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.Indigo;
+            this.button7.Location = new System.Drawing.Point(3, 415);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(273, 200);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lavender;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Indigo;
+            this.button6.Location = new System.Drawing.Point(588, 209);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(287, 200);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Lavender;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Indigo;
+            this.button5.Location = new System.Drawing.Point(282, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(295, 200);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Lavender;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Indigo;
+            this.button4.Location = new System.Drawing.Point(3, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(273, 200);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lavender;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Indigo;
+            this.button3.Location = new System.Drawing.Point(588, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(287, 200);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lavender;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Indigo;
+            this.button2.Location = new System.Drawing.Point(282, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 200);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lavender;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Indigo;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 200);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.AliceBlue;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(1228, 12);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(186, 70);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::X_si_Zero_eu.Properties.Resources.a8054bd49daa169530dcb99b0c606a66;
+            this.ClientSize = new System.Drawing.Size(1426, 891);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ X si 0/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_si_Zero_eu
+{
+    public partial class Form1 : Form
+    {
+        const int BSIZE = 3;
+        const string Player1 = "X";
+        const string Player2 = "O";
+
+        string[,] tablaMatrice = new string[BSIZE, BSIZE];
+        string currentPlayer = Player1;
+        public Form1()
+        {
+            InitializeComponent();
+            resetTabla();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control cnt in tableLayoutPanel1.Controls)
+            {
+                Button btn = (Button)cnt;
+                btn.Click += Btn_Click;
+            }
+            resetTabla();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            /*sender este de tipul object deoarece poate fi orice obiect care a
+             * declanșat evenimentul (în cazul nostru, un buton). 
+             * Însă, pentru a putea accesa proprietățile și metodele specifice 
+             * unui buton, trebuie să facem cast la tipul Button.*/
+            Button button = (Button)sender;
+            int linie = tableLayoutPanel1.GetRow(button);
+            int coloana = tableLayoutPanel1.GetColumn(button);
+
+            if (tablaMatrice[linie, coloana] == null)//nu merge ==0 pt ca e de tip string
+            {
+                tablaMatrice[linie, coloana] = currentPlayer;
+                button.Text = currentPlayer;
+               
+                if (isWinner(currentPlayer))
+                {
+                    MessageBox.Show("Winner: " + currentPlayer);
+                    resetTabla();
+                }
+                else if(isFull())
+                {
+                    MessageBox.Show("of");
+                    resetTabla();
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer == Player1) ? Player2 : Player1;
+                }
+            }
+
+        }
+
+        private void resetTabla()
+        {
+            tablaMatrice = new string[BSIZE, BSIZE];
+            foreach (Control cont in tableLayoutPanel1.Controls)
+            {
+                Button bt = (Button)cont;
+                bt.Text = "";
+            }
+            currentPlayer = Player1;
+        }
+
+        private bool isWinner(string player)
+        {
+            for (int i = 0; i < BSIZE; i++)
+            {
+                if (tablaMatrice[i, 0] == player && tablaMatrice[i, 1] == player
+                    && tablaMatrice[i, 2] == player)
+                    return true;
+            }
+            for (int j = 0; j < BSIZE; j++)
+            {
+                if (tablaMatrice[0, j] == player && tablaMatrice[1, j] == player
+                    && tablaMatrice[2, j] == player)
+                    return true;
+            }
+            if (tablaMatrice[0, 0] == player && tablaMatrice[1, 1] == player && tablaMatrice[2, 2] == player)
+            {
+                return true;
+            }
+            else if (tablaMatrice[0, 2] == player && tablaMatrice[1, 1] == player && tablaMatrice[2, 0] == player)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isFull()
+        {
+            foreach (string cell in tablaMatrice)
+            {
+                if (cell == null)
+                    return false;
+            }
+            return true;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+
+}
